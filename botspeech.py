56a03238362b99d9cec46b9d2905701f2dade28f
Nikita Fedorov <nikitafedorovv@gmail.com>
2020-07-21 01:51:49 +0300

    Fix bugs

--- botspeech.py
+++ botspeech.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
-START_MESSAGE_TEXT = 'Hello.\n\n— Send a text to set the heading\n— Choose or send a picture to set background picture' \
-                     ' (you can use your own picture from device)' \
-                     '\n— Send a float from 0.0 to 1.0 to set blackout (0.6 usually looks nice)' \
-                     '\n— Send positive integer number to set blur (recommended values are from 1 to 20)' \
-                     '\n\nIf you do not want to set any heading just send a symbol \'.\''
+START_MESSAGE_TEXT = 'Hello.\n\n— Send any text to set the heading;' \
+                     '\n— Choose or send a picture to set the background picture' \
+                     ' (you can use your own picture from device or use a cache below);' \
+                     '\n— Send a float from 0.0 to 1.0 to set blackout (0.6 usually looks nice);' \
+                     '\n— Send positive integer number to set blur (recommended values are from 1 to 20);' \
+                     '\n\nTip: You can send a \'.\' symbol to set an empty heading;' \
+                     '\nTip number two: Use usual line breaks to divide lines in the heading'
 WAIT_FOR_AN_IMAGE_MESSAGE_TEXT = "One moment... ⏳"
 SUCCESSFULLY_REMOVED_ANSWER = '✅ Successfully removed from saved backgrounds'
 ALREADY_REMOVED_ANSWER = 'ℹ️ Seems like it is already removed'
