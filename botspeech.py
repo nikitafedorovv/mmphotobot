1f286f75e71bf3fa09da52dbb54c686351819866
Nikita Fedorov <nikitafedorovv@gmail.com>
2020-07-21 02:59:15 +0300

    Edit greetings speech

--- botspeech.py
+++ botspeech.py
@@ -5,8 +5,10 @@ START_MESSAGE_TEXT = 'Hello!\n\n— <b>Send</b> me any text to set the heading;'
                      ' (you can use your own picture from device or use a cache below);' \
                      '\n— <b>Send</b> me a float from 0.0 to 1.0 to set blackout (0.6 usually looks nice);' \
                      '\n— <b>Send</b> me a positive integer number to set blur (recommended values are from 1 to 20).' \
-                     '\n\n\n<i>Tip</i>:\nYou can send a \'.\' symbol to set an empty heading.' \
-                     '\n\n<i>Tip #2</i>:\nUse usual line breaks to divide lines of the heading.'
+                     '\n\n\n<i>Tip</i>:\nYou can send me your own image from your device to use it as' \
+                     ' the background.' \
+                     '\n\n<i>Tip #2</i>:\nYou can send a \'.\' symbol to set an empty heading.' \
+                     '\n\n<i>Tip #3</i>:\nUse usual line breaks to divide lines of the heading.'
 WAIT_FOR_AN_IMAGE_MESSAGE_TEXT = "One moment... ⏳"
 SUCCESSFULLY_REMOVED_ANSWER = '✅ Successfully removed from saved backgrounds'
 ALREADY_REMOVED_ANSWER = 'ℹ️ Seems like it is already removed'
